9a7e7c59399028366c755638929b9dec84a5b742
Nathan Batista <na.than.fjv@gmail.com>
2026-01-21 21:01:49 +0000

    feat: create banners route

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,10 @@
 import { Router, Response } from 'express';
+import * as bannerController from '../controllers/banner';
 
 export const routes = Router();
 
 routes.get('/ping', (_, res: Response) => {
   res.json({ pong: true });
 });
+
+routes.get('/banners', bannerController.getBanners);
